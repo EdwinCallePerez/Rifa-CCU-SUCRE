97355debfbc8464516478c50f7286d1b48e6897b
EdwinCallePerez <edwincalleperez.is@gmail.com>
2024-12-13 01:01:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function elegirGanador() {
     };
 
     // Iniciar la animación
-    intervalo = setInterval(animarGlobos, 8);
+    intervalo = setInterval(animarGlobos, 12);
 
     // Seleccionar el ganador después de un tiempo
     setTimeout(() => {
@@ -145,7 +145,7 @@ function elegirGanador() {
             ganadorBtn.style.display = 'none';
             obtenerOtroBtn.style.display = 'none';
         }
-    }, 7000); // Esperamos 3 segundos para elegir el ganador
+    }, 10000); // Esperamos 3 segundos para elegir el ganador
 }
 
 // Función para aceptar al ganador
